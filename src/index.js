60eae9b79ad75d146c7f613c3033a49fa3e73cfb
Dhyey1811 <dhyeypatel9640@gmail.com>
2025-03-28 12:34:01 -0400

    new update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppWrapper from './AppWrapper';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import App from './App';
 
+// Create an Apollo Client instance
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql', // Your GraphQL server URL
+  cache: new InMemoryCache(), // In-memory cache for Apollo
+});
+
+// Wrap your App component with ApolloProvider to provide Apollo Client to your app
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppWrapper />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
