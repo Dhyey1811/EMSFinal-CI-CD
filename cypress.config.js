a50bc929d05836874fae0863981dc592a8096802
Dhyey1811 <70282937+Dhyey1811@users.noreply.github.com>
2025-04-11 14:15:29 -0400

    Delete cypress.config.js

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000', // Your frontend URL
-    supportFile: 'cypress/support/e2e.js',
-    specPattern: 'cypress/e2e/**/*.js',
-  },
-});
