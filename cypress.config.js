b02af3aa533e9b2be906461afc07b994e569f93c
Dhyey1811 <dhyeypatel9640@gmail.com>
2025-04-16 13:19:27 -0400

    updated cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000', // Your frontend URL
+    baseUrl: 'http://localhost:3000/', // Your frontend URL
     supportFile: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.js',
   },
